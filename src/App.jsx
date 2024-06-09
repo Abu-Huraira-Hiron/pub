611217b3bae3a4e906a87d3ae597a8b99acdbbd4
unknown <fakirabuhuraira@gmail.com>
2024-06-09 16:43:23 +0600

    tailwind css add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from "./Components/Navbar"
 import Afnav from "./Components/Afnav"
-import ss from "./Components/ss"
+import ss from "./Components/ss.jsx"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Afnav/>
-      <ss />
+      <ss/>
     </div>
   )
 }
