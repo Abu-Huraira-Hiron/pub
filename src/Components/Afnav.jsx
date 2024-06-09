515fc4eb8fd7c93c7dccfb35b7a0c9f576fbcee5
unknown <fakirabuhuraira@gmail.com>
2024-06-09 09:28:50 +0600

    first commit

---
+++ src/Components/Afnav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Afnav.css'
+
+const Afnav = () => {
+  return (
+    <div className='Afnav'>
+      <div className='Afnav-text'>
+        <h1>we ensure better educaton for betterworld</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Afnav
