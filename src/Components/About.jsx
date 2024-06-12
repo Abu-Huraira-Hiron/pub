8306aa9e10e3b02dbf5ffa4af0ebd3e7c07171f5
unknown <fakirabuhuraira@gmail.com>
2024-06-12 11:00:23 +0600

    gij

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Aboutus.css'
-
+import abu from './assets/abu'
 const About = () => {
   return (
-    <div>
-      
+    <div className='abu'>
+      <div className='abu'>
+        <img src="abu" alt="" srcset="" />
+
+      </div>
     </div>
   )
 }
